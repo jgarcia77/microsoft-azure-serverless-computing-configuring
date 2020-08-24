ba128cd51a80f0d34d213895c46c926767bb076d
JoshGa <JoshGa@JOSHGA-PC>
2020-08-24 07:09:17 -0500

    first commit

---
+++ 05/demos/ServiceBusQueues/ServiceBusQueues/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+
+namespace ServiceBusQueues
+{
+    class Program
+    {
+        const string ServiceBusConnectionString = "<your connection string>";
+        const string QueueName = "<your queue name>";
+        static IQueueClient queueClient;
+
+        static void Main(string[] args)
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync()
+        {
+            // await SendMessages();
+            await ReceiveMessages();
+        }
+
+        private static async Task ReceiveMessages()
+        {
+            queueClient = new QueueClient(ServiceBusConnectionString, QueueName);
+            // Register the queue message handler and receive messages in a loop
+            RegisterOnMessageHandlerAndReceiveMessages();
+            Console.ReadKey();
+            await queueClient.CloseAsync();
+        }
+
+        static void RegisterOnMessageHandlerAndReceiveMessages()
+        {
+            var messageHandlerOptions = new MessageHandlerOptions(ExceptionReceivedHandler)
+            {
+                MaxConcurrentCalls = 1,
+                AutoComplete = false
+            };
+            queueClient.RegisterMessageHandler(ProcessMessagesAsync, messageHandlerOptions);
+        }
+
+        static async Task ProcessMessagesAsync(Message message, CancellationToken token)
+        {
+            Console.WriteLine($"Received message: SequenceNumber:{message.SystemProperties.SequenceNumber} Body:{Encoding.UTF8.GetString(message.Body)}");
+            await queueClient.CompleteAsync(message.SystemProperties.LockToken);
+        }
+        static Task ExceptionReceivedHandler(ExceptionReceivedEventArgs exceptionReceivedEventArgs)
+        {
+            Console.WriteLine($"Message handler encountered an exception {exceptionReceivedEventArgs.Exception}.");
+            var context = exceptionReceivedEventArgs.ExceptionReceivedContext;
+            Console.WriteLine("Exception context for troubleshooting:");
+            Console.WriteLine($"- Endpoint: {context.Endpoint}");
+            Console.WriteLine($"- Entity Path: {context.EntityPath}");
+            Console.WriteLine($"- Executing Action: {context.Action}");
+            return Task.CompletedTask;
+        }
+
+        private static async Task SendMessages()
+        {
+            const int numberOfMessages = 10;
+            queueClient = new QueueClient(ServiceBusConnectionString, QueueName);
+            // Send messages.
+            await SendMessagesAsync(numberOfMessages);
+            Console.ReadKey();
+            await queueClient.CloseAsync();
+        }
+
+        static async Task SendMessagesAsync(int numberOfMessagesToSend)
+        {
+            try
+            {
+                for (var i = 0; i < numberOfMessagesToSend; i++)
+                {
+                    // Create a new message to send to the queue.
+                    string messageBody = $"Message {i}";
+                    var message = new Message(Encoding.UTF8.GetBytes(messageBody));
+
+                    // Write the body of the message to the console.
+                    Console.WriteLine($"Sending message: {messageBody}");
+
+                    // Send the message to the queue.
+                    await queueClient.SendAsync(message);
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"{DateTime.Now} :: Exception: {exception.Message}");
+            }
+        }
+    }
+}
---
+++ 05/demos/ServiceBusTopicsAndSubs/ServiceBusTopicsAndSubs/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+
+namespace ServiceBusTopicsAndSubs
+{
+    class Program
+    {
+        const string ServiceBusConnectionString = "<your connection string>";
+        const string TopicName = "<topic name>";
+        static ITopicClient topicClient;
+
+        const string SubscriptionName = "<subscription name>";
+        static ISubscriptionClient subscriptionClient;
+
+        static void Main(string[] args)
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync()
+        {
+            // await SendMessages();
+            await ReceiveMessages();
+        }
+        private static async Task ReceiveMessages()
+        {
+            subscriptionClient = new SubscriptionClient(ServiceBusConnectionString, TopicName, SubscriptionName);
+            // Register subscription message handler and receive messages in a loop
+            RegisterOnMessageHandlerAndReceiveMessages();
+            Console.ReadKey();
+            await subscriptionClient.CloseAsync();
+        }
+
+        private static void RegisterOnMessageHandlerAndReceiveMessages()
+        {
+            var messageHandlerOptions = new MessageHandlerOptions(ExceptionReceivedHandler)
+            {
+                MaxConcurrentCalls = 1,
+                AutoComplete = false
+            };
+
+            subscriptionClient.RegisterMessageHandler(ProcessMessagesAsync, messageHandlerOptions);
+        }
+
+        static async Task ProcessMessagesAsync(Message message, CancellationToken token)
+        {
+            Console.WriteLine($"Received message: SequenceNumber:{message.SystemProperties.SequenceNumber} Body:{Encoding.UTF8.GetString(message.Body)}");
+            await subscriptionClient.CompleteAsync(message.SystemProperties.LockToken);
+        }
+
+        static Task ExceptionReceivedHandler(ExceptionReceivedEventArgs exceptionReceivedEventArgs)
+        {
+            Console.WriteLine($"Message handler encountered an exception {exceptionReceivedEventArgs.Exception}.");
+            var context = exceptionReceivedEventArgs.ExceptionReceivedContext;
+            Console.WriteLine("Exception context for troubleshooting:");
+            Console.WriteLine($"- Endpoint: {context.Endpoint}");
+            Console.WriteLine($"- Entity Path: {context.EntityPath}");
+            Console.WriteLine($"- Executing Action: {context.Action}");
+            return Task.CompletedTask;
+        }
+
+        private static async Task SendMessages()
+        {
+            const int numberOfMessages = 10;
+            topicClient = new TopicClient(ServiceBusConnectionString, TopicName);
+            // Send messages.
+            await SendMessagesAsync(numberOfMessages);
+            Console.ReadKey();
+            await topicClient.CloseAsync();
+        }
+
+        static async Task SendMessagesAsync(int numberOfMessagesToSend)
+        {
+            try
+            {
+                for (var i = 0; i < numberOfMessagesToSend; i++)
+                {
+                    // Create a new message to send to the topic.
+                    string messageBody = $"Message {i}";
+                    var message = new Message(Encoding.UTF8.GetBytes(messageBody));
+
+                    // Write the body of the message to the console.
+                    Console.WriteLine($"Sending message: {messageBody}");
+
+                    // Send the message to the topic.
+                    await topicClient.SendAsync(message);
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"{DateTime.Now} :: Exception: {exception.Message}");
+            }
+        }
+
+    }
+}
